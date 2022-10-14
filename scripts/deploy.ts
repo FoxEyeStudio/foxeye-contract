a25c3eee1d0e3e37dc82df03aa133afcca93451a
FireLancer <firelancer@foxeye.io>
2022-10-14 17:15:42 +0800

    fix deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,15 +3,16 @@ import { writeAddr } from "./utils/addressHandler";
 
 
 async function main() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_MONTH_IN_SECS = 30 * 24 * 60 * 60;
+    const TOTAL_DEADLINE = 365 * 24 * 60 * 60; //ONE_YEAR_IN_SECS
     // const SEASON_DURATION = 15 * 24 * 60 * 60; // half a month
-    const TEST_SEASON_DURATION = 30 * 60;
+    const TEST_SEASON_DURATION = 30 * 60; // half an hour
     const feeReceiver = '0x12C33E63551E9dc8A073edBdeeDf04fC98B944E4'; // replace this address in production
     
-    // const STABLE_COIN_ADDR = '0x47c4845d5E43507638Acd5553EE42132175D5656'; // Rinkeby test USDT
     // const STABLE_COIN_ADDR = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'; // BUSD on BSC
     const fUSD_COIN_ADDR = '0x620553CcB76f665F036659654fa4c692aA108E95'; // test USD on BSC
+    
+    
+    const LOTTERY_FEE = ethers.BigNumber.from(30).mul(ethers.constants.WeiPerEther);
 
 
     const FoxToken = await ethers.getContractFactory("FoxToken");
@@ -20,12 +21,12 @@ async function main() {
     console.log(`FoxToken deployed to ${foxToken.address}`);
 
     const DividendsPool = await ethers.getContractFactory("DividendsPool");
-    const dividendsPool = await DividendsPool.deploy(feeReceiver,20, 10 ,30 ,100 ,foxToken.address ,fUSD_COIN_ADDR);
+    const dividendsPool = await DividendsPool.deploy(feeReceiver,20, 10 ,LOTTERY_FEE ,100 ,foxToken.address ,fUSD_COIN_ADDR);
     await dividendsPool.deployed();
     console.log(`DividendsPool deployed to ${dividendsPool.address}`);
 
     const Airdrop = await ethers.getContractFactory("Airdrop");
-    const airdrop = await Airdrop.deploy(ONE_YEAR_IN_SECS, TEST_SEASON_DURATION ,foxToken.address);
+    const airdrop = await Airdrop.deploy(TOTAL_DEADLINE, TEST_SEASON_DURATION ,foxToken.address);
     await airdrop.deployed();
     console.log(`Airdrop deployed to ${airdrop.address}`);
 
